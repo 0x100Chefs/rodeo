e06886b84272f6d415404d6e8436cde09e54172e
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-01-01 11:36:45 +0100

    feat: add config parser

--- src/main.rs
+++ src/main.rs
@@ -1,156 +1,44 @@
-use axum::response::IntoResponse;
-use axum::routing::{get, post};
-use axum::Router;
-
-use hyper::body::Bytes;
-use hyper::{Body, Client, Request, Response, StatusCode, Uri};
-use hyper::{HeaderMap, Method};
-
-use std::fmt;
-use std::net::SocketAddr;
-use tower_http::cors::{Any, CorsLayer};
-use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-
+use clap::{arg, command, value_parser, ArgAction, Command};
+use errors::Error;
+use std::path::PathBuf;
+mod errors;
 mod parsers;
-
-#[tokio::main]
-async fn main() -> Result<(), hyper::Error> {
-    // load env variables and the service configuration
-
-    dotenv::dotenv().ok();
-
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG")
-                .unwrap_or_else(|_| "martus-proxy=debug,tower_http=debug".into()),
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init(); // allow debugging in development set up
-
-    // define cors scope as any
-    let cors_layer = CorsLayer::new().allow_headers(Any).allow_methods([
-        Method::GET,
-        Method::POST,
-        Method::DELETE,
-        Method::PUT,
-        Method::PATCH,
-    ]); // restrict methods
-
-    // build our application with a route to match all HTTP verbs
-    let app = Router::new()
-        .route(
-            "/v1/*path", // match all request method
-            post(handler)
-                .get(handler)
-                .patch(handler)
-                .put(handler)
-                .delete(handler),
+fn main() {
+    let matches = command!()
+        .arg(
+            arg!( -c --config <FILE> "The config file path")
+                .required(false)
+                .value_parser(value_parser!(PathBuf)),
         )
-        .route("/", get(health_check))
-        .layer(cors_layer)
-        .fallback(handle_404);
-
-    // run it
-    let addr = SocketAddr::from(([0, 0, 0, 0], 5000));
-    println!("server running on {address}", address = addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-}
-
-#[derive(Debug, Default)]
-struct Proxy {
-    pub headers: HeaderMap,
-    pub method: Method,
-    pub path: Uri,
-    pub body: Body,
-}
-
-// impl display
-impl fmt::Display for Proxy {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "headers {:#?}\nmethod: {}\npath: {}\nbody:{:#?}\n",
-            self.headers, self.method, self.path, self.body
+        .subcommand(
+            Command::new("init")
+                .about("start a new proxy server in the specified path")
+                // .arg(arg!(-p --path "the rodeo.toml | rodeo.json path").action(ArgAction::SetTrue))
+                .arg(arg!([file] "the config file type, default to toml")),
         )
-    }
-}
+        .get_matches();
 
-struct ServicePath;
-
-#[allow(dead_code)]
-impl ServicePath {
-    // create a new service
-    pub fn from(service_id: &str, service_base_url: String) -> (String, String) {
-        let service_id = service_id.to_string();
-        let service_base_url = service_base_url.to_string();
-
-        (service_id, service_base_url)
+    // the init command
+    if let Some(matches) = matches.subcommand_matches("init") {
+        if let Some(config_type) = matches.get_one::<String>("file") {
+            parsers::create_config_file(String::from(config_type)).expect("TODO: panic message");
+            std::process::exit(0)
+        };
     }
 
-    // parse the url
-    pub fn parse_url(path: Uri) -> String {
-        // split the path to extract service ID
-        let path = path.path().split('/').collect::<Vec<&str>>();
+    // let Some(config_path) = matches.get_one::<PathBuf>("config") else {
+    //     Error::new("missing config file, please specify path to rodeo.toml or rodeo.json").missing_config();
+    //     std::process::exit(0);
+    //
+    // };
 
-        // detect the recipient server
-        let service_id = path[2];
-        let resource_path = &path[3..].join("/");
-
-        let service = parsers::parse_config(service_id).unwrap();
-        let service_base_url = service.base_url; // SERVING THE REQUEST TO THE PROXY SERVER WOULD RETURN A 404 ERROR SINCE NO ROUTE WOULD BE MATCHED
-
-        let request_url = format!("{service_base_url}{resource_path}");
-
-        request_url
-    }
-    // read the url from env
-    fn from_env<'a>(key: &'a str, default: &'a str) -> std::string::String {
-        std::env::var(key).unwrap_or(default.to_string())
-    }
-}
-
-/// 404 handler
-async fn handle_404() -> impl IntoResponse {
-    (
-        StatusCode::NOT_FOUND,
-        axum::response::Json(serde_json::json!({
-        "success":false,
-        "message":String::from("The requested resource does not exist on this server!"),
-        })),
-    )
-}
-
-/// health check
-async fn health_check() -> impl IntoResponse {
-    (
-        StatusCode::OK,
-        axum::response::Json(serde_json::json!({
-        "success":true,
-        "message":String::from("The server is up and running!"),
-        })),
-    )
-}
-
-// `Request` gives you the whole request for maximum control
-async fn handler(path: Uri, method: Method, headers: HeaderMap, body: Bytes) -> Response<Body> {
-    // pass data to request builder
-    let body = Body::from(body);
-    let url = ServicePath::parse_url(path);
-    let mut req = Request::builder();
-
-    // add the header to the built request object
-    for (key, value) in headers {
-        req.headers_mut().unwrap().insert(key.unwrap(), value);
-    }
-
-    let req = req.method(method).uri(&url).body(body).unwrap();
-    let client = Client::new();
-    let res = client.request(req).await;
-
-    // map response to return type
-    //TODO: improve error handling
-    res.unwrap()
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    // let subcommand = matches.subcommand_matches("init");
+    // let config_path = matches.get_one::<PathBuf>("config");
+    //
+    // if config_path.is_none() {
+    //     //     Error::new("missing config file, please specify path to rodeo.toml or rodeo.json").missing_config();
+    //     std::process::exit(0);
+    // }
 }
--- src/parsers.rs
+++ src/parsers.rs
@@ -1,40 +1,37 @@
-// use the fs module to read proxy.toml
-use serde::{Deserialize, Serialize};
-use toml::Table;
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Service {
-    name: String,
-    version: String,
-    pub base_url: String,
-}
-
-impl Service {
-    pub fn new(name: &str, base_url: &str, version: &str) -> Self {
-        Self {
-            name: name.to_string(),
-            base_url: base_url.to_string(),
-            version: version.to_string(),
-        }
+use crate::errors::Error;
+use std::fmt::format;
+use std::fs::File;
+use std::io::prelude::*;
+pub fn create_config_file(file_type: String) -> Result<(), String> {
+    if file_type != "json" && file_type != "toml" {
+       return  Err("config file can only be json or toml".to_string())
     }
-}
-pub fn parse_config(service_id: &str) -> Result<Service, String> {
-    // read the service configuration, or throw error
-    // let Some(config) = fs::read_to_string("./proxy.toml").ok() else {
-    //     return Err(String::from("error reading config"));
-    // };
-    let config = std::include_bytes!("../proxy.toml");
-    let config = String::from_utf8_lossy(config);
-    //  convert the file into Table destructure provided by Toml parser
-    let config = config.parse::<Table>().unwrap();
-    let Some(service) = &config["services"].get(service_id) else {
-        return Err(String::from("error parsing config"));
-    };
+    let file_name = format!("rodeo.{file_type}");
+    let mut file = File::create(file_name).unwrap();
 
-    // convert to Services Struct
-    let name = service_id;
-    let version = format!("v{}", service.get("version").unwrap().as_str().unwrap());
-    let base_url = service.get("base_url").unwrap().as_str().unwrap();
+    if file_type == "json"{
+        file.write_all(br#"
+        {
+        "services" :[
+        {"path":"", "base_url" :""}
+        ]
+        }
+        "#).unwrap();
+    }
 
-    Ok(Service::new(name, base_url, &version))
+    if file_type == "json"{
+        file.write_all(br#"
+    port = 5000
+timeout = '10s'
+
+[services]
+admin = {version = "1", base_url = "http://0.0.0.0:5003"}
+auth = {version = "1", base_url = "http://0.0.0.0:5001/"}
+student = {version = "1", base_url = "http://0.0.0.0:5002"}
+
+        "#).unwrap();
+    }
+
+    // file.write_all(b"Hello, world!")?;
+    Ok(())
 }
